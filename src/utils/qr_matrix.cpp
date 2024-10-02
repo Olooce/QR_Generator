4ee9e3aa69f0cd0f8661b38e6e5c8fca221b77b6
Olooce <asirastephen9@gmail.com>
2024-10-02 03:40:02 +0300

    chore: Update .idea/misc.xml, Temporary/LastTest.log, utils/qr_matrix.cpp
    modified: .idea/misc.xml
    modified: Temporary/LastTest.log
    modified: utils/qr_matrix.cpp

--- src/utils/qr_matrix.cpp
+++ src/utils/qr_matrix.cpp
@@ -10,8 +10,56 @@ QRMatrix::QRMatrix(int version) : version(version), size(21 + 4 * (version - 1))
 }
 
 void QRMatrix::addPositionMarkers() {
-    // TODO: Implement logic to add the position markers (7x7 modules each)
+    // Define the pattern for a 7x7 position marker
+    std::vector<std::vector<int>> marker = {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1}
+    };
+
+    // Function to place a position marker at a given (row, col)
+    auto placeMarker = [&](int row, int col) {
+        for (int y = 0; y < 7; ++y) {
+            for (int x = 0; x < 7; ++x) {
+                matrix[row + y][col + x] = marker[y][x];
+            }
+        }
+    };
+
+    // Place position markers at three corners
+    placeMarker(0, 0);                   // Top-left
+    placeMarker(0, size - 7);            // Top-right
+    placeMarker(size - 7, 0);            // Bottom-left
+
+    // Fill in the separator area (1 module space around the markers)
+    auto fillSeparator = [&](int rowStart, int colStart, int width, int height) {
+        for (int y = rowStart; y < rowStart + height; ++y) {
+            for (int x = colStart; x < colStart + width; ++x) {
+                if (x >= 0 && x < size && y >= 0 && y < size && matrix[y][x] == -1) {
+                    matrix[y][x] = 0;
+                }
+            }
+        }
+    };
+
+    // Top-left marker separator
+    fillSeparator(0, 7, 8, 1);       // Horizontal line below
+    fillSeparator(7, 0, 1, 8);       // Vertical line to the right
+
+    // Top-right marker separator
+    fillSeparator(0, size - 8, 8, 1);    // Horizontal line below
+    fillSeparator(7, size - 8, 1, 8);    // Vertical line to the left
+
+    // Bottom-left marker separator
+    fillSeparator(size - 8, 0, 1, 8);    // Vertical line to the right
+    fillSeparator(size - 8, 7, 8, 1);    // Horizontal line above
 }
+
+
 
 void QRMatrix::placeData(const std::vector<int>& dataBits) {
     //TODO: Implement logic to place data bits and error correction bits on the matrix
